42a20f189d0e260c9fa9c5a3ad39508055090742
Yuheng Su <gipsyh.icu@gmail.com>
2022-10-19 15:15:46 +0000

    transfer display code into single file

---
+++ src/display.rs
@@ -0,0 +1,76 @@
+use crate::{Aig, AigEdge, AigNode, AigNodeType};
+use std::fmt::Display;
+
+impl Display for AigNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.typ {
+            AigNodeType::True => write!(f, "True"),
+            AigNodeType::PrimeInput => write!(f, "PI{}", self.id),
+            AigNodeType::LatchInput => write!(f, "LI{}", self.id),
+            AigNodeType::And(_, _) => write!(f, "A{}", self.id),
+        }
+    }
+}
+
+impl Display for AigEdge {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.complement {
+            write!(f, "!")?;
+        }
+        Ok(())
+    }
+}
+
+impl Display for Aig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "==================")?;
+        writeln!(f, "input num: {}", self.num_inputs,)?;
+        writeln!(f, "latch num: {}", self.latchs.len())?;
+        writeln!(f, "output num: {}", self.outputs.len())?;
+        writeln!(f, "and num: {}", self.num_ands)?;
+        writeln!(f, "bad state num: {}", self.bads.len())?;
+        writeln!(f, "------------------")?;
+        write!(f, "cinputs:")?;
+        for ci in &self.cinputs {
+            write!(f, " {}", self.nodes[*ci])?;
+        }
+        writeln!(f, "\n------------------")?;
+        for and in self.ands_iter() {
+            let fanin0 = and.fanin0();
+            let fanin1 = and.fanin1();
+            writeln!(
+                f,
+                "{} = {}{} & {}{}",
+                self.nodes[and.node_id()],
+                fanin0,
+                self.nodes[fanin0.node_id()],
+                fanin1,
+                self.nodes[fanin1.node_id()]
+            )?;
+        }
+        writeln!(f, "------------------")?;
+        writeln!(f, "outputs:")?;
+        for idx in 0..self.outputs.len() {
+            writeln!(
+                f,
+                "O{}: {}{}",
+                idx + 1,
+                self.outputs[idx],
+                self.nodes[self.outputs[idx].node_id()]
+            )?;
+        }
+        writeln!(f, "------------------")?;
+        writeln!(f, "bad states:")?;
+        for idx in 0..self.bads.len() {
+            writeln!(
+                f,
+                "B{}: {}{}",
+                idx + 1,
+                self.bads[idx],
+                self.nodes[self.bads[idx].node_id()]
+            )?;
+        }
+        writeln!(f, "==================")?;
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(assert_matches)]
 
 mod aiger;
+mod display;
 mod eliminate;
 mod fraig;
 mod migrate;
@@ -115,17 +116,6 @@ impl AigNode {
     }
 }
 
-impl Display for AigNode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.typ {
-            AigNodeType::True => write!(f, "True"),
-            AigNodeType::PrimeInput => write!(f, "PI{}", self.id),
-            AigNodeType::LatchInput => write!(f, "LI{}", self.id),
-            AigNodeType::And(_, _) => write!(f, "A{}", self.id),
-        }
-    }
-}
-
 impl Into<AigEdge> for AigNode {
     fn into(self) -> AigEdge {
         AigEdge::new(self.id, false)
@@ -158,15 +148,6 @@ impl AigEdge {
 
     pub fn compl(&self) -> bool {
         self.complement
-    }
-}
-
-impl Display for AigEdge {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.complement {
-            write!(f, "!")?;
-        }
-        Ok(())
     }
 }
 
@@ -388,60 +369,6 @@ impl Index<AigNodeId> for Aig {
 
     fn index(&self, index: AigNodeId) -> &Self::Output {
         &self.nodes[index]
-    }
-}
-
-impl Display for Aig {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "==================")?;
-        writeln!(f, "input num: {}", self.num_inputs,)?;
-        writeln!(f, "latch num: {}", self.latchs.len())?;
-        writeln!(f, "output num: {}", self.outputs.len())?;
-        writeln!(f, "and num: {}", self.num_ands)?;
-        writeln!(f, "bad state num: {}", self.bads.len())?;
-        writeln!(f, "------------------")?;
-        write!(f, "cinputs:")?;
-        for ci in &self.cinputs {
-            write!(f, " {}", self.nodes[*ci])?;
-        }
-        writeln!(f, "\n------------------")?;
-        for and in self.ands_iter() {
-            let fanin0 = and.fanin0();
-            let fanin1 = and.fanin1();
-            writeln!(
-                f,
-                "{} = {}{} & {}{}",
-                self.nodes[and.node_id()],
-                fanin0,
-                self.nodes[fanin0.node_id()],
-                fanin1,
-                self.nodes[fanin1.node_id()]
-            )?;
-        }
-        writeln!(f, "------------------")?;
-        writeln!(f, "outputs:")?;
-        for idx in 0..self.outputs.len() {
-            writeln!(
-                f,
-                "O{}: {}{}",
-                idx + 1,
-                self.outputs[idx],
-                self.nodes[self.outputs[idx].node_id()]
-            )?;
-        }
-        writeln!(f, "------------------")?;
-        writeln!(f, "bad states:")?;
-        for idx in 0..self.bads.len() {
-            writeln!(
-                f,
-                "B{}: {}{}",
-                idx + 1,
-                self.bads[idx],
-                self.nodes[self.bads[idx].node_id()]
-            )?;
-        }
-        writeln!(f, "==================")?;
-        Ok(())
     }
 }
 
